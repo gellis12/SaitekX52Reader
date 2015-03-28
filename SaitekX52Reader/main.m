a220e7af2ad58cde36a146eaea739ab5fd5405c8
Gordon Ellis <gellis12@gmail.com>
2015-03-28 04:11:42 -0700

    Initial Commit

---
+++ SaitekX52Reader/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SaitekX52Reader
+//
+//  Created by Gordon Ellis on 2015-03-28.
+//  Copyright (c) 2015 Gordon Ellis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
